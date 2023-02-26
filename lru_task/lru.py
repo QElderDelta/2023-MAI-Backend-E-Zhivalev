d3fc512fd2f4dc96b1f42a24f0506b41ac4f3759
Егор Живалев <egorzhivalev@gmail.com>
2023-02-26 19:48:26 +0300

    Add LRU cache implementation

--- lru_task/lru.py
+++ lru_task/lru.py
@@ -1,12 +1,35 @@
+from collections import OrderedDict
+
 class LRUCache:
     def __init__(self, capacity: int=10) -> None:
-        pass
+        self._capacity = capacity
+        self._cache = OrderedDict()
+
+    def _update_on_use(self, key):
+        if key not in self._cache:
+            return
+
+        value = self._cache[key]
+
+        self._cache.pop(key)
+
+        self._cache[key] = value
 
     def get(self, key: str) -> str:
-        return ''       
+        if key not in self._cache:
+            return ''
+
+        self._update_on_use(key)
+
+        return self._cache[key]
 
     def set(self, key: str, value: str) -> None:
-        pass
+        self._update_on_use(key)
+
+        self._cache[key] = value
+
+        if len(self._cache) > self._capacity:
+            self._cache.popitem(last=False)
 
     def rem(self, key: str) -> None:
-        pass
+        self._cache.pop(key)
