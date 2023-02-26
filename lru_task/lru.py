921e092299d4ccc7d7df710aef264106ae85105b
Егор Живалев <egorzhivalev@gmail.com>
2023-02-26 19:17:25 +0300

    Add boilerplate file and tests

---
+++ lru_task/lru.py
@@ -0,0 +1,12 @@
+class LRUCache:
+    def __init__(self, capacity: int=10) -> None:
+        pass
+
+    def get(self, key: str) -> str:
+        return ''       
+
+    def set(self, key: str, value: str) -> None:
+        pass
+
+    def rem(self, key: str) -> None:
+        pass
---
+++ lru_task/tests/lru_test.py
@@ -0,0 +1,38 @@
+from lru import LRUCache
+import pytest
+
+@pytest.fixture
+def test_cache():
+    cache = LRUCache(3)
+
+    cache.set('key1', 'value1')
+    cache.set('key2', 'value2')
+    cache.set('key3', 'value3')
+
+    return cache
+
+def test_cache_size_exceeded(test_cache):
+    test_cache.set('key4', 'value4')
+
+    assert test_cache.get('key1') == ''
+    assert test_cache.get('key4') == 'value4'
+
+def test_correct_update_logic(test_cache):
+    assert test_cache.get('key1') == 'value1'
+
+    test_cache.set('key4', 'value4')
+
+    assert test_cache.get('key1') == 'value'
+    assert test_cache.get('key4') == 'value4'
+    assert test_cache.get('key2') == ''
+
+def test_get_set_rem_methods(test_cache):
+    assert test_cache.get('key1') == 'value1'
+
+    test_cache.set('key1', 'value4')
+
+    assert test_cache.get('key1') == 'value4'
+
+    test_cache.rem('key1')
+
+    assert test_cache.get('key1') == ''
